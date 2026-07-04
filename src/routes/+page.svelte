d15b1fa6fd22354115e25c340e1c11fed10ae317
Nerobrain <nerobrain@gmail.com>
2026-07-04 16:38:23 +0700

    Init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { resolve } from "$app/paths";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<a href={resolve("/demo/paraglide")}>paraglide</a>
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+const base = process.env.BASE_PATH || "";
+const normalizedBase = base && (!base.startsWith("/") ? `/${base}` : base);
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        sveltekit({
+            compilerOptions: {
+                // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+                runes: ({ filename }) =>
+                    filename.split(/[/\\]/).includes("node_modules")
+                        ? undefined
+                        : true,
+            },
+            adapter: adapter({
+                precompress: true,
+                fallback: "404.html",
+            }),
+            paths: {
+                base: process.argv.includes("dev")
+                    ? ""
+                    : (normalizedBase as "" | `/${string}`),
+            },
+        }),
+        paraglideVitePlugin({
+            project: "./project.inlang",
+            outdir: "./src/lib/paraglide",
+        }),
+    ],
+    server: {
+        watch: {
+            // Включаем опрос файлов вместо использования системных событий
+            usePolling: true,
+            // Интервал опроса в миллисекундах (меньше — быстрее, но больше нагрузка)
+            interval: 500,
+            // Если папка с проектом большая, можно исключить node_modules из опроса
+            ignored: ["**/node_modules/**"],
+        },
+    },
+});
