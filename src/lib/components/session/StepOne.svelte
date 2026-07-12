5aef21cb754a0720e70bb431f79e80ecb220b858
Nerobrain <nerobrain@gmail.com>
2026-07-12 16:38:56 +0700

    fix: Добавлено поддержка отображения ассоциаций с шагами и улучшена логика работы счётчиков и прогрессбара

--- src/lib/components/session/StepOne.svelte
+++ src/lib/components/session/StepOne.svelte
@@ -12,8 +12,8 @@
     let step = $state(analysisStore.step);
 
     const hints = [
-        "Записывайте первое слово, которое прийдёт в голову.",
-        "Не анализируйте. Не ищите «правильный» ответ.",
+        "Записывайте первое слово или словосочетание, которое прийдёт в голову.",
+        "Не анализируйте. Не ищите «правильный» ответ. Старайтесь не повторяться",
     ];
     const theme = analysisStore.theme;
 
@@ -38,7 +38,39 @@
             goNext();
         }
     }
+
+    function getSubstep(
+        step: number,
+        limit: number,
+    ): { row: number; rowLimit: number; offset: number; substep: number } {
+        const limit_1 = limit; // 16
+        const limit_2 = limit_1 / 2; // 8
+        const limit_3 = limit_2 / 2; // 4
+        const limit_4 = limit_3 / 2; // 2
+        const limit_5 = limit_4 / 2; // 1
+        const offset_1 = 0;
+        const offset_2 = offset_1 + limit_1; // 0 + 16 = 16
+        const offset_3 = offset_2 + limit_2; // 16 + 8 = 24
+        const offset_4 = offset_3 + limit_3; // 24 + 4 = 28
+        const offset_5 = offset_4 + limit_4; // 28 + 2 = 30
+
+        if (step > offset_5) {
+            return { row: 5, rowLimit: limit_5, offset: offset_5, substep: step - offset_5 };
+        } else if (step > offset_4) {
+            return { row: 4, rowLimit: limit_4, offset: offset_4, substep: step - offset_4 };
+        } else if (step > offset_3) {
+            return { row: 3, rowLimit: limit_3, offset: offset_3, substep: step - offset_3 };
+        } else if (step > offset_2) {
+            return { row: 2, rowLimit: limit_2, offset: offset_2, substep: step - offset_2 };
+        }
+
+        return { row: 1, rowLimit: limit_1, offset: offset_1, substep: step - offset_1 };
+    }
+
+    let { rowLimit, offset, substep } = $derived(getSubstep(step, limit));
 </script>
+
+<!-- {$inspect(" row=", row, " step=", step, " offset=", offset, " rowLimit=", rowLimit, " substep=", substep)} -->
 
 <div class="mb-4">
     <p>Тема</p>
@@ -49,7 +81,7 @@
     </p>
 </div>
 
-<div class="mb-4 h-14" id="lastone">
+<div class="mb-4 h-14">
     {#if step > 1 && step <= limit}
         <p>Предыдущее слово</p>
         <p
@@ -57,8 +89,21 @@
         >
             {step > 1 ? `${analysisStore.ansvers[step - 2]}` : ""}
         </p>
-    {:else if step > limit}
-        <p><strong class="text-indigo-600">Что объединяет</strong> эти два слова?</p>
+    {:else}
+        <p><strong>Что объединяет</strong> эти два слова?</p>
+        <div class="flex inset-s-full gap-1">
+            <p
+                class="w-fit py-1 px-3 bg-indigo-50 dark:bg-indigo-950 border border-indigo-200 dark:border-indigo-800 rounded-lg"
+            >
+                {analysisStore.ansvers[offset - rowLimit * 2 + substep * 2 - 2]}
+            </p>
+            <p class="my-auto">и</p>
+            <p
+                class="w-fit py-1 px-3 bg-indigo-50 dark:bg-indigo-950 border border-indigo-200 dark:border-indigo-800 rounded-lg"
+            >
+                {analysisStore.ansvers[offset - rowLimit * 2 + substep * 2 - 1]}
+            </p>
+        </div>
     {/if}
 </div>
 
@@ -81,7 +126,7 @@
 
 <Hint text={hints} />
 
-<Progressbar title="Ассоциация {step} из {limit}" {step} {limit} />
+<Progressbar title="Ассоциация {substep} из {rowLimit}" step={substep} limit={rowLimit} />
 
 <div class="flex justify-between mt-4">
     <button
